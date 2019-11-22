c4de9958dedc9d9f1ebbb46cbd942c126fa6188f
Hackman9912 <charleshackett89@gmail.com>
2019-11-22 15:31:08 -0600

    Added more gui stuff

--- Hackett/test.py
+++ Hackett/test.py
@@ -1,6 +1,33 @@
-import sys
+import tkinter as tk
+from tkinter import ttk
 import os
+import sys
+import re
+class MainMenu(tk.Tk):
+    def __init__(self, master=None, list1):
+        tk.Tk.__init__(self, master)
+        self.geometry('400x400')
+        self.title('Method definition')
+        self.var = tk.IntVar(self)
+        self.drop_menu(list1)
 
-import tkinter as tkinter
+    def drop_menu(self, list1):
+        
+        self.comboExample = ttk.Combobox(list1)
+        self.comboExample.configure(state = 'readonly')
+        self.comboExample.grid(row=0, column=0)
 
-tk.Frame
+    # def grab_and_assign(self, event):
+    #     chosen_option = self.var.get()
+    #     label_chosen_variable = tk.Label(self, text=chosen_option)
+    #     label_chosen_variable.grid(row=1, column=2)
+
+def main():
+
+    app = MainMenu()
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+
