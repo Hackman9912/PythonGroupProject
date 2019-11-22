5dafd16604323ab12d528a672f61abcb7199088f
Philip Cammarata <philip@cammarata.me>
2019-11-22 15:27:28 -0600

    gui time.

--- cammarata/app.py
+++ cammarata/app.py
@@ -2,12 +2,15 @@ import os
 import re
 import sys
 import tkinter as tk
+from tkinter import ttk
 
 class MainWindow(tk.Tk):
     def __init__(self, packages):
         super().__init__()
         self.packages = packages
-        self.package_selected = tk.IntVar()
+        self.package_selected = tk.StringVar()
+        self.package_combobox = self.create_package_combobox(self.packages)
+        self.package_combobox.pack()
 
         self.title('Hackman Dictionary Search')
         self.geometry('500x400')
@@ -19,6 +22,12 @@ class MainWindow(tk.Tk):
         pattern = re.compile(r'[a-z]')
         for package in packages:
             self.package_methods.update({package: [method for method in dir(package) if pattern.match(method)]})
+    
+    def create_package_combobox(self, packages):
+        return ttk.Combobox(self,
+                            state='readonly',
+                            textvariable=self.package_selected,
+                            values=self.packages)
 
 if __name__ == "__main__":
     # which packages will be go into?
