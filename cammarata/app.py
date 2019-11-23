9b9cc1f562399dad08a08d3749665ba58943c24f
Philip Cammarata <philip@cammarata.me>
2019-11-23 11:37:23 -0600

    gettin'g sheet done.

--- cammarata/app.py
+++ cammarata/app.py
@@ -5,36 +5,45 @@ import tkinter as tk
 from tkinter import ttk
 
 class MainWindow(tk.Tk):
-    def __init__(self, packages):
+    def __init__(self, modules):
         super().__init__()
-        self.packages = packages
-        self.package_selected = tk.StringVar()
-        self.package_combobox = self.create_package_combobox(self.packages)
-        self.package_combobox.pack()
-
         self.title('Hackman Dictionary Search')
         self.geometry('500x400')
+    
+        self.modules = modules 
+        # because we pass the module itself we need to get it's string name 
+        self.module_combobox = self.create_combobox(self.modules)
+        self.module_combobox.bind('<<ComboboxSelected>>', self.populate_module_functions_combobox)
+        self.module_combobox.pack()
 
-        self.package_methods = {}
-        self.package_method_select = tk.IntVar()
-
-        # methods/functions will only start with a lowercase a-z.
+        # functions will only start with a lowercase a-z.
         pattern = re.compile(r'[a-z]')
-        for package in packages:
-            self.package_methods.update({package: [method for method in dir(package) if pattern.match(method)]})
+        self.module_functions = {}
+        for module in self.modules:
+            # have to eval(module) as it's a string and not the actual module
+            self.module_functions[module] = [function for function in dir(eval(module)) if pattern.match(function)]
+        self.module_functions_combobox = self.create_combobox()
+        self.module_functions_combobox.bind('<<ComboboxSelected>>', self.get_function_definition)
+        self.module_functions_combobox.pack()
 
-        print(self.package_selected)
+    def populate_module_functions_combobox(self, event):
+        # clear module_functions' combobox if a new module has been selected
+        self.module_functions_combobox.set('')
+        self.module_functions_combobox['values'] = self.module_functions[self.module_combobox.get()]
 
-    def create_package_combobox(self, packages):
+    def get_function_definition(self, event):
+        module = self.module_combobox.get()
+        function = self.module_functions_combobox.get()
+        help(f"{module}.{function}")
+
+    def create_combobox(self, values=[]):
         return ttk.Combobox(self,
                             state='readonly',
-                            textvariable=self.package_selected,
-                            values=self.packages)
-
+                            values=values)
 
 if __name__ == "__main__":
-    # which packages will be go into?
-    packages = ['os', 'sys']
+    # add the module to the list as a string
+    modules = ['os', 'sys']
 
-    main_window = MainWindow(packages)
+    main_window = MainWindow(modules)
     main_window.mainloop()
