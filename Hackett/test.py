9ede5a534a1696e8e54ba7d9774200dbcd07e30f
Hackman9912 <rsjakob@gmail.com>
2019-11-24 00:31:18 -0600

    Updated the program, it works but needs prettier

--- Hackett/test.py
+++ Hackett/test.py
@@ -1,42 +1,59 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import scrolledtext as tkst
 import os
 import sys
 import re
 class MainMenu(tk.Tk):
-    def __init__(self, list1, list2, master=None):
+    def __init__(self, list1, master=None):
         tk.Tk.__init__(self, master)
-        self.geometry('400x400')
+        self.geometry('800x400')
         self.title('Method definition')
-        self.var = tk.IntVar(self)
-        self.drop_menu(list1, 'Libraries', 0)
-        self.drop_menu(list2, 'Methods', 1)
+        self.list2 = []
+        self.os_dict = None 
+        self.drop_menu1 = self.drop_menu(list1, 'Libraries', 0)
+        self.drop_menu1.bind("<<ComboboxSelected>>", self.get_methods)
+        self.drop_menu2 = self.drop_menu(self.list2, 'Methods', 2)
+        self.drop_menu2.bind("<<ComboboxSelected>>", self.get_choice)
+        self.textbox = self.gettext()
 
     def drop_menu(self, list1, var1, var2):
         self.comboExample = ttk.Combobox(self, values = list1, state = 'readonly', height = 10)
         self.comboExample.set(var1)
-        self.comboExample.configure(state = 'readonly')
         self.comboExample.grid(row = 0, column = var2)
-        self.comboExample.bind("<<ComboboxSelected>>", lambda: self.var_get(self.comboExample.get()))
+        return self.comboExample
 
-    def var_get(self, var1):
-        chosen_option = var1
-        label_chosen_variable = tk.Label(self, text=chosen_option)
-        label_chosen_variable.grid(row=1, column=2)
+    def gettext(self):
+        self.text = tkst.ScrolledText(self, wrap = tk.WORD, width = 85, height = 20, name = 'defs')
+        self.text.grid(padx = 10, pady = 10, row = 2, columnspan = 3, sticky = tk.W + tk.E)
+        self.text.insert('1.0', '')
+        return self.text
 
+    def get_choice(self, event):
+        self.meth = self.drop_menu2.get()
+        if self.libr != 'Libraries' and self.meth != 'Methods':
+            self.textbox.destroy()
+            self.textbox = self.gettext()
+            self.textbox.insert('1.0', self.os_dict[self.meth])
 
+    def get_methods(self, event):
+        self.list2 = []
+        self.libr = self.drop_menu1.get()
+        if self.libr == 'os':
+            self.os_dict = os.__dict__
+        elif self.libr == 'sys':
+            self.os_dict = sys.__dict__
+        elif self.libr == 're':
+            self.os_dict = re.__dict__
+        for k, v in self.os_dict.items():
+            self.list2.append(k)
+        self.drop_menu2.configure(values = self.list2)
 
 
 def main():
-    os_dict = os.__dict__
     list1 = ['os', 'sys', 're']
-    list2 = []
-    for k, v in os_dict.items():
-        list2.append(k)
-    app = MainMenu(list1, list2)
+    app = MainMenu(list1)
     app.mainloop()
 
-
-if __name__ == '__main__':
-    main()
+main()
 
