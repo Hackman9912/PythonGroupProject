7522b348495ed15f9d829ba0085d70d5aea4c764
Philip Cammarata <philip@cammarata.me>
2019-11-25 13:33:22 -0600

    tag it.

--- cammarata/app.py
+++ cammarata/app.py
@@ -6,17 +6,18 @@ from tkinter import ttk
 
 class MainWindow(tk.Tk):
     def __init__(self, modules):
-        ### Window Setup
         super().__init__()
-        self.title('Cammarata Dictionary Search')
-        self.geometry('500x400')
 
+        ### Window Setup ###
+        self.title('Cammarata Dictionary Search')
+        self.geometry('600x500')
+
+        # setup initial dropdown values
         self.modules = modules 
         self.functions = {}
-
         # generate functions for a modules {'module1': ['function1','function2','...']}
         # functions will only start with a lowercase a-z.
-        pattern = re.compile(r'[a-z]')
+        pattern = re.compile(r'[a-zA-Z]')
         for module in self.modules:
             # dynamically import the module and dir it to grab the functions
             imported_module = importlib.import_module(module)
@@ -24,17 +25,15 @@ class MainWindow(tk.Tk):
 
 
         ### Module Dropdown ###
-        self.module_combobox = self.create_combobox(self.modules)
-        self.module_combobox.set('Modules')
-        # callback to populate_functions_combobox when a module is selected
-        self.module_combobox.bind('<<ComboboxSelected>>', self.populate_functions_combobox)
-        self.module_combobox.pack()
+        self.modules_combobox = self.create_combobox(self.modules)
+        self.modules_combobox.set('Modules')
+        self.modules_combobox.bind('<<ComboboxSelected>>', self.populate_functions_combobox)
+        self.modules_combobox.pack()
 
 
         ### Functions Dropdown ###
         self.functions_combobox = self.create_combobox()
         self.functions_combobox.set('Functions')
-        # callback to populate_function_definition when a function is selected
         self.functions_combobox.bind('<<ComboboxSelected>>', self.populate_function_definition)
         self.functions_combobox.pack()
 
@@ -43,20 +42,19 @@ class MainWindow(tk.Tk):
         self.definitions_textbox = self.create_textbox()
         self.definitions_textbox.pack()
 
+
     def create_combobox(self, values=[]):
         return ttk.Combobox(self, state='readonly', values=values)
 
     def create_textbox(self):
-        # i expected more to put here
         return tk.Text(self)
 
     def populate_functions_combobox(self, event):
-        # clear module_functions' combobox if a new module has been selected
         self.functions_combobox.set('Functions')
-        self.functions_combobox['values'] = self.functions[self.module_combobox.get()]
+        self.functions_combobox['values'] = self.functions[self.modules_combobox.get()]
 
     def populate_function_definition(self, event):
-        module = self.module_combobox.get()
+        module = self.modules_combobox.get()
         function = self.functions_combobox.get()
 
         # don't forget to render the help text into something that isn't garbo to display in Tkinter
