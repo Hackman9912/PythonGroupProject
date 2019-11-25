60556fd5190bf54b76d6496f2e2998b89f847e56
Philip Cammarata <philip@cammarata.me>
2019-11-25 13:13:50 -0600

    updates.

--- cammarata/app.py
+++ cammarata/app.py
@@ -11,31 +11,40 @@ class MainWindow(tk.Tk):
         self.title('Cammarata Dictionary Search')
         self.geometry('500x400')
 
-        ### Module Dropdown ###
         self.modules = modules 
+        self.functions = {}
+
+        # generate functions for a modules {'module1': ['function1','function2','...']}
+        # functions will only start with a lowercase a-z.
+        pattern = re.compile(r'[a-z]')
+        for module in self.modules:
+            # dynamically import the module and dir it to grab the functions
+            imported_module = importlib.import_module(module)
+            self.functions[module] = [function for function in dir(imported_module) if pattern.match(function)]
+
+
+        ### Module Dropdown ###
         self.module_combobox = self.create_combobox(self.modules)
         self.module_combobox.set('Modules')
         # callback to populate_functions_combobox when a module is selected
         self.module_combobox.bind('<<ComboboxSelected>>', self.populate_functions_combobox)
         self.module_combobox.pack()
 
+
         ### Functions Dropdown ###
-        # functions will only start with a lowercase a-z.
-        pattern = re.compile(r'[a-z]')
-        self.functions = {}
-        for module in self.modules:
-            # dynamically import the module and dir it to grab the functions
-            imported_module = importlib.import_module(module)
-            self.functions[module] = [function for function in dir(imported_module) if pattern.match(function)]
         self.functions_combobox = self.create_combobox()
         self.functions_combobox.set('Functions')
-
         # callback to populate_function_definition when a function is selected
         self.functions_combobox.bind('<<ComboboxSelected>>', self.populate_function_definition)
         self.functions_combobox.pack()
 
+
+        ### Functions Dropdown ###
         self.definitions_textbox = self.create_textbox()
         self.definitions_textbox.pack()
+
+    def create_combobox(self, values=[]):
+        return ttk.Combobox(self, state='readonly', values=values)
 
     def create_textbox(self):
         # i expected more to put here
@@ -56,9 +65,6 @@ class MainWindow(tk.Tk):
         # you don't set a textbox, you delete and insert.
         self.definitions_textbox.delete(1.0, tk.END)
         self.definitions_textbox.insert(tk.END, definition)
-
-    def create_combobox(self, values=[]):
-        return ttk.Combobox(self, state='readonly', values=values)
 
 if __name__ == "__main__":
     # add the module to the list as a string
